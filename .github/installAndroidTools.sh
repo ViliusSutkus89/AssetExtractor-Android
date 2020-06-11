49bba3155779e706171f8ec317ee8e85839148b1
Vilius Sutkus <ViliusSutkus89@gmail.com>
2020-06-11 15:49:45 +0300

    [CI/CD] Download build-tools;30.0.0 and platforms;android-30

--- .github/installAndroidTools.sh
+++ .github/installAndroidTools.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -eu
 
+ANDROID_SDK_TOOLS=6514223
+BUILD_TOOLS="build-tools;30.0.0"
+PLATFORM="platforms;android-30"
+
+
 SDK_MGR=$ANDROID_HOME/tools/bin/sdkmanager
 if test ! -f $SDK_MGR; then
   SDK_MGR=$ANDROID_HOME/cmdline-tools/tools/bin/sdkmanager
 fi
 
 if test ! -f $SDK_MGR; then
+  echo "Installing cmdline tools"
   cmdtoolsfile=commandlinetools-linux-${ANDROID_SDK_TOOLS}_latest.zip
   if test ! -f ${cmdtoolsfile}
   then
@@ -15,5 +21,10 @@ if test ! -f $SDK_MGR; then
   sudo unzip -d $ANDROID_HOME/cmdline-tools ${cmdtoolsfile} > /dev/null
 fi
 
-echo "y" | sudo $SDK_MGR "tools" > /dev/null
+echo "Installing ${BUILD_TOOLS}"
+echo "y" | sudo $SDK_MGR "${BUILD_TOOLS}" > /dev/null
+
+echo "Installing ${PLATFORM}"
+echo "y" | sudo $SDK_MGR "${PLATFORM}" > /dev/null
+
 echo "y" | sudo $SDK_MGR --licenses > /dev/null
